6880621a0009bee6b32fca1ac77e8b4c9fffb4d8
Abhinav-AI-dev <abhinavp822@gmail.com>
2026-05-07 20:23:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from sklearn.preprocessing import StandardScaler
 # =====================================================
 st.set_page_config(
     page_title="AI Shopping Recommendation System",
-    layout="wide"
-    initial_sidebar_state = "expanded"
+    layout="wide",
+    initial_sidebar_state="expanded"
 )
 
 # =====================================================
